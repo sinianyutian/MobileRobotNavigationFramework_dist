6b4b05bf51d27bc340459844647d04f8d8f473dd
Yuki Suga <ysuga@ysuga.net>
2015-06-18 10:38:15 +0900

    update for jyaml library

--- Mapper_ALL.bat
+++ Mapper_ALL.bat
@@ -1,4 +1,4 @@
 set PATH=%~dp0\bin;%PATH%
 start bin\rtcd.exe -f conf\mapper_rtc_cpp.conf
-java -cp bin\rtcd.jar;bin\commons-cli-1.1.jar;bin\OpenRTM-aist-1.1.0.jar;bin/jyaml-1.3.jar rtcd.rtcd -f conf\mapper_rtc_java.conf
+java -cp bin\rtcd.jar;bin\commons-cli-1.1.jar;bin\OpenRTM-aist-1.1.0.jar;bin\jyaml-1.3.jar rtcd.rtcd -f conf\mapper_rtc_java.conf
 
